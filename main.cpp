f34fcf2e3d853cd60c3917e80bef2cc28c09b6b8
brantcass <94635676+brantcass@users.noreply.github.com>
2022-01-29 13:45:27 -0600

    Added while loop to count for the function string_length in functions.cpp. Added a for loop in main.cpp for the length of the parameter to increment the I value and then print the number of characters.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ Program: This file is for the functions used in functions.cpp
 ****************/
 
 #include "main.h"
+using namespace std;
 
 int main(int argc, char** argv) {
     cout << "My Command line analyzer..." << endl;
 
     cout << "The number of command line parameters are: "<< argc << endl;
 
+
     if(argc > 1){
-        std::cout<< " The length of the first parameter is "<< string_length(argv[1]) << std::endl;
+        std::cout<< "The length of the name of the program is: "<< string_length(argv[1]) << std::endl;
+    }
+    
+    
+    for(int i = 1 ; i < argc ;i++){
+        cout<<"The length of the parameter is "<< i << " is " << string_length(argv[i])<<endl;
+
     }
 
 
