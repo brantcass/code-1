0b195127f856fb51a8849cfff55d60a87b1de235
brantcass <94635676+brantcass@users.noreply.github.com>
2022-01-29 13:46:24 -0600

    Added the while loop to the string_length function to return the incremented index.

--- functions.cpp
+++ functions.cpp
@@ -5,9 +5,21 @@ Program: This file is for the functions used in functions.cpp
 ****************/
 
 #include "functions.h"
+#include <iostream>
+#include <iomanip>
 
 
 int string_length(const char *str){
-    return 0;
+
+    int index = 0;
+
+    while(str[index] && str[index] != 0){
+        
+        index ++;
+
+        
+    }
+    return index;
+    
 
 }
--- functions.h
+++ functions.h
@@ -5,8 +5,11 @@ Program: This file is for the functions used in functions.cpp
 ****************/
 #ifndef COMMANLINE_FUNCTIONS_H
 #define COMMANDLINE_FUNCTIONS_H
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
-int string_length(const char*);
+int string_length(const char* line);
 
 
 #endif // COMMANDLINE_FUNCTIONS_H/
